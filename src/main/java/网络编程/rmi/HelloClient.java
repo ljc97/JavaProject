7ebeb855df3d7b26fbfbab6478228a424a98d83f
linjc <2573456222@qq.com>
2019-07-17 17:36:55 +0800

    test1

--- src/main/java/网络编程/rmi/HelloClient.java
+++ src/main/java/网络编程/rmi/HelloClient.java
@@ -6,14 +6,14 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 /**
- *  客户端程序
- *  在客户端调用远程对象上的远程方法，并返回结果。
+ * 客户端程序
+ * 在客户端调用远程对象上的远程方法，并返回结果。
  */
 public class HelloClient {
-    public static void main(String []args) {
+    public static void main(String[] args) {
         try {
             //在RMI服务注册表中查找名称为RmiHello的对象，并调用其上的方法
-            IHello localHello = (IHello)Naming.lookup("rmi://localhost:8888/RmiHello");
+            IHello localHello = (IHello) Naming.lookup("rmi://localhost:8888/RmiHello");
             System.out.println(localHello.sayHello());
             System.out.println(localHello.sayHelloTo("zzz"));
         } catch (NotBoundException e) {
--- src/main/java/网络编程/rmi/IHello.java
+++ src/main/java/网络编程/rmi/IHello.java
@@ -6,10 +6,11 @@ import java.rmi.RemoteException;
 /**
  * 定义一个远程接口，必须继承Remote接口，其中需要远程调用的方法必须抛出RemoteException异常
  */
-public interface IHello extends Remote{
+public interface IHello extends Remote {
 
     //远程调用的方法必须有返回值，不能为void
     //必须抛出RemoteException异常,否认则Client运行时会抛出RemoteException异常
     String sayHello() throws RemoteException;
+
     String sayHelloTo(String somebody) throws RemoteException;
 }
