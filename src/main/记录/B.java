e8517aa7935f4d8efdb5f0a106fdc2be94a44f31
linjc <2573456222@qq.com>
2019-08-27 17:00:11 +0800

    test

---
+++ src/main/记录/B.java
@@ -0,0 +1,23 @@
+/**
+ * @Description: //TODO
+ * @Author: 林经翀（jingchong.lin@ucarinc.com）
+ * @Date 2019/8/20 16:51
+ * @Version 1.0
+ */
+public class B
+{
+
+    public static B t1 = new B();
+    public static B t2 = new B();
+    {
+        System.out.println("构造块");
+    }
+    static
+    {
+        System.out.println("静态块2");
+    }
+    public static void main(String[] args)
+    {
+        B t = new B();
+    }
+}
