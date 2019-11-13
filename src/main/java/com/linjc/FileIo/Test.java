59574c5b72a1fad4e2ba211e7846b86c4c5fe325
林经翀 <jingchong.lin@ucarinc.com>
2019-11-13 10:14:03 +0800

    测试分支

---
+++ src/main/java/com/linjc/FileIo/Test.java
@@ -0,0 +1,13 @@
+package com.linjc.FileIo;
+
+/**
+ * @Description:
+ * @Author: 林经翀（jingchong.lin@ucarinc.com）
+ * @Date 2019/11/13 10:12
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1/0);
+    }
+}
