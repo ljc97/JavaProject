08b7b9ff46f45b9436bdfb25ff0a457ed4610a77
林经翀 <jingchong.lin@ucarinc.com>
2020-01-13 15:35:22 +0800

    测试分支

--- src/main/java/com/test.java
+++ src/main/java/com/test.java
@@ -1,10 +1,13 @@
 package com;
 
 /**
- * @Description: testssssss
+ * @Description:
  * @Author: 林经翀（jingchong.lin@ucarinc.com）
  * @Date 2020/1/13 15:20
  * @Version 1.0
  */
 public class test {
+    public static void main(String[] args) {
+
+    }
 }
